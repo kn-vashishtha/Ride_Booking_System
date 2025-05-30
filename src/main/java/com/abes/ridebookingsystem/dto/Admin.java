b789c5e333075ba0939e47b8dc0e4c9097e0e5e4
Anamika Verma <anamikaverma1412@gmail.com>
2025-05-30 10:44:26 +0530

    Admin dto by anamika

--- src/main/java/com/abes/ridebookingsystem/dto/Admin.java
+++ src/main/java/com/abes/ridebookingsystem/dto/Admin.java
@@ -1,4 +1,24 @@
-package com.abes.ridebookingsystem.dto;
+package com.abes.ridebookingsystem.dto;           // Package Declaration
 
-public class Admin {
+public class Admin extends User {                 // Admin class extends User(inheriting its fields and methods)
+	private String role;                         // Additional field specific to Admin class that not present in User
+
+	public Admin(String userId, String name, String email, String password, String role) { // Constructor
+		super(userId, name, email, password);   // Call to the parent class User constructor to initialize inherited fields
+		this.role = role;                       // Set Admin Role
+	} 
+
+	public String getRole() {                  //Getter method 
+		return role;
+	}
+
+	public void setRole(String role) {        // Setter method 
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {                // Overriding the toString() method
+		return "Admin{" + "userId='" + userId + '\'' + ", name='" + name + '\'' + ", email='" + email + '\'' // Customize the string
+				+ ", role='" + role + '\'' + '}';
+	}
 }
